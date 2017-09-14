4cf273ed86181e13828347e5a689d5ccb4bc2773
hlrd93 <hlrd_93@hotmail.com>
2017-09-14 11:56:04 -0400

    devise model update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+  	devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :lastname, :country, :city, :summary, :email, :password, :current_password,:avatar)}
+  end
 end
